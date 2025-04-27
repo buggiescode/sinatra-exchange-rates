d9ca838434cb8888f462ccecdb61893466e71c7d
buggiescode <137247769+buggiescode@users.noreply.github.com>
2025-04-27 07:47:52 +0000

    listed currencies

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "dotenv/load"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_KEY")}"
+
+  raw_response = HTTP.get(api_url)
+
+  raw_string = raw_response.to_s
+
+  parsed_data = JSON.parse(raw_string)
+
+  currencies = parsed_data.fetch("currencies")
+
+  @cur_abb = currencies.keys()
+
+  erb(:homepage)
+
 end
