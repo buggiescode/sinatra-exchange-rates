dc448d19c39ed94ed9c03c1b7850eb4577f4c6e2
buggiescode <137247769+buggiescode@users.noreply.github.com>
2025-05-01 02:31:26 +0000

    finished up rates

--- app.rb
+++ app.rb
@@ -4,6 +4,34 @@ require "http"
 require "dotenv/load"
 
 get("/") do
+  @cur_abb = get_currencies()
+
+  erb(:homepage)
+
+end
+
+get("/:exchange_from") do
+  @cur_abb = get_currencies()
+  @original_currency = params.fetch("exchange_from")
+
+  erb(:convert_to)
+end
+
+get("/:exchange_from/:exchange_to") do
+  @original_currency = params.fetch("exchange_from")
+  @destination_currency = params.fetch("exchange_to")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  raw_response = HTTP.get(api_url)
+  raw_string = raw_response.to_s
+  parsed_data = JSON.parse(raw_string)
+
+  @exchange_rate = parsed_data.fetch("result")
+
+  erb(:result)
+end
+
+def get_currencies()
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_KEY")}"
 
   raw_response = HTTP.get(api_url)
@@ -14,8 +42,5 @@ get("/") do
 
   currencies = parsed_data.fetch("currencies")
 
-  @cur_abb = currencies.keys()
-
-  erb(:homepage)
-
+  return currencies.keys()
 end
